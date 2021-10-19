dd9bcb530a469b3929eae782acad1cb694a34298
siddhesh <siddeshmhatre1649@gmail.com>
2021-10-19 17:18:38 +0530

    ..

---
+++ src/components/todo.js
@@ -0,0 +1,126 @@
+import React, { useState } from "react";
+import book from '../components/book.png'
+const Todo = () => {
+  const [inputData, setInputData] = useState("");
+  const [items, setItems] = useState([]);
+  const [toggleSubmit, setToggleSubmit] = useState(true);
+  const [isEditItem, setisEditItem] = useState(null);
+
+  const addItem = () => {
+    if (!inputData) {
+      alert("plzz fill data");
+    } else if (inputData && !toggleSubmit) {   //for edit
+      setItems(
+        items.map((elem) => {
+          if (elem.id === isEditItem) {
+            return { ...elem, name: inputData };
+          }
+          return elem;
+        })
+        
+      );
+      setToggleSubmit(true);
+      setInputData("");
+      setisEditItem(null);
+    } else {
+      const allInputData = {
+        id: new Date().getTime().toString(),
+        name: inputData,
+      };
+
+      setItems([...items, allInputData]);
+      setInputData("");
+    }
+  };
+
+  const deleteItem = (id) => {
+    const updateditems = items.filter((elem) => {
+      return elem.id !== id;
+    });
+    setItems(updateditems);
+  };
+
+  const removeAll = () => {
+    setItems([]);
+  };
+
+  const editItem = (id) => {
+    let newEditItem = items.find((elem) => {
+      return elem.id === id;
+    });
+    // console.log(newEditItem);
+    setToggleSubmit(false);
+    setInputData(newEditItem.name);
+    setisEditItem(id);
+  };
+  return (
+    <>
+      <div className="main-div">
+        <div className="child-div">
+          <figure>
+            <img src={book} alt="todologo" />
+            <figcaption>Add your list here 📑</figcaption>
+          </figure>
+          <div className="addItems">
+            <input
+              type="text"
+              name=""
+              value={inputData}
+              onChange={(e) => setInputData(e.target.value)}
+              placeholder=" ✍️ Add items"
+            />
+            {toggleSubmit ? (
+              <i
+                className="fa fa-plus add-btn"
+                title="add item"
+                onClick={addItem}
+              ></i>
+            ) : (
+              <i
+                className="fas fa-edit add-btn"
+                title="Update item"
+                onClick={addItem}
+              ></i>
+            )}
+          </div>
+          <div className="showItems">
+            {items.map((elem) => {
+              return (
+                <div className="eachItem" key={elem.id}>
+                  <h3>{elem.name}</h3>
+
+                  <div className="todo-btn">
+                    <i
+                      class="fas fa-edit add-btn"
+                      title="Edit item"
+                      onClick={() => editItem(elem.id)}
+                    ></i>
+
+                    <i
+                      className="far fa-trash-alt add-btn"
+                      title="Delete item"
+                      onClick={() => deleteItem(elem.id)}
+                    ></i>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+
+          <div className="showItems">
+            <button
+              className="btn effect04"
+              data-sm-link-text="Remove All"
+              onClick={removeAll}
+            >
+              {" "}
+              <span>CHECK List</span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Todo;
